d91e4d9b2e2bd8f26da36ab088bbba9a6456266b
Аникеев Николай <nikolaian@halykbank.kz>
2024-01-08 21:17:28 +0600

    added: .env to .gitignore

--- server/src/services/mongo.service.js
+++ server/src/services/mongo.service.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL =
-  "mongodb+srv://nasa-api:C9zg8xnHNC3eneTC@nasacluster.gkq7rnl.mongodb.net/nasa?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL;
+
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
 });
